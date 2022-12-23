fd4ec0423b1001309579b1cbff5c696ce0a06d48
Minhhhh <mineskiroxor@gmail.com>
2022-12-24 06:26:31 +0800

    update deploy script

--- scripts/upgrade.js
+++ scripts/upgrade.js
@@ -3,42 +3,62 @@ const { ethers, upgrades } = require("hardhat");
 require("dotenv").config();
 const fs = require('fs');
 const envfile = require('envfile');
+const { assert } = require("console");
 const sourcePath = './output.txt';
+const assert = require('assert');
 
-const content = fs.readFileSync(sourcePath);
-const lines = content.toString().split('\n');
+const getKeyPair = () => {
+  const content = fs.readFileSync(sourcePath);
+  const lines = content.toString().split('\n');
 
-const key_pair = {};
+  const key_pair = {};
 
-for (let line = 0; line < lines.length; line++) {
-  const currentline = lines[line].split('=');
-  key_pair[currentline[0]] = currentline[1];
+  for (let line = 0; line < lines.length; line++) {
+    const currentline = lines[line].split('=');
+    key_pair[currentline[0]] = currentline[1];
+  }
+
+  return key_pair;
 }
 
-const proxyAddress = key_pair["PROXY_ADDRESS"];
-const oldImplAddress = key_pair["IMPLEMENTATION_ADDRESS"];
+
+const key_pair = getKeyPair();
+
 
 async function main() {
+  const proxyAddress = key_pair["PROXY_ADDRESS"];
+  const oldImplAddress = key_pair["IMPLEMENTATION_ADDRESS"];
+
   console.log(proxyAddress,"-Proxy address")
   console.log(oldImplAddress,"-Old Impl address")
 
   const CrowdFundingV2 = await ethers.getContractFactory("CrowdFundingV2");
-  console.log("Upgrade to CrowdFundingV2...");
+  console.log("Upgrading to CrowdFundingV2...");
 
   const crowdFundingV2 = await upgrades.upgradeProxy(proxyAddress, CrowdFundingV2);
+  console.log("Upgraded...");
+
+  let newImplementationAddress = await upgrades.erc1967.getImplementationAddress(crowdFundingV2.address);
+  let newAdminAddress = await upgrades.erc1967.getAdminAddress(crowdFundingV2.address);
+
+  while(newImplementationAddress == oldImplAddress) {
+    newImplementationAddress = await upgrades.erc1967.getImplementationAddress(crowdFundingV2.address);
+    newAdminAddress = await upgrades.erc1967.getAdminAddress(crowdFundingV2.address);
+  }
 
   console.log(crowdFundingV2.address, "-ProxyAddress");
-  console.log(await upgrades.erc1967.getImplementationAddress(crowdFundingV2.address),"-ImplementationAddress");
-  console.log(await upgrades.erc1967.getAdminAddress(crowdFundingV2.address),"-AdminAddress");
+  console.log(newImplementationAddress,"-ImplementationAddress");
+  console.log(newAdminAddress, "-AdminAddress");
 
   // Write to output.txt
   let parsedFile = envfile.parse(sourcePath);
   parsedFile.PROXY_ADDRESS = crowdFundingV2.address;
-  parsedFile.IMPLEMENTATION_ADDRESS = await upgrades.erc1967.getImplementationAddress(crowdFundingV2.address);
-  parsedFile.ADMIN_ADDRESS = await upgrades.erc1967.getAdminAddress(crowdFundingV2.address);
+  parsedFile.IMPLEMENTATION_ADDRESS = newImplementationAddress;
+  parsedFile.ADMIN_ADDRESS = newAdminAddress;
 
   fs.writeFileSync('./output.txt', envfile.stringify(parsedFile));
 }
+
 
 main().catch((error) => {
   console.error(error);
