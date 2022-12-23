12e28ecd69af3aaf82c08f404897d32aa2ff6850
Minhhhh <mineskiroxor@gmail.com>
2022-12-24 04:16:51 +0800

    add deployment for upgrade contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,25 @@
 const hre = require("hardhat");
 const { ethers, upgrades } = require("hardhat");
+const fs = require('fs');
+const envfile = require('envfile');
+const sourcePath = './output.txt';
 
 async function main() {
   const CrowdFundingV1 = await ethers.getContractFactory("CrowdFundingV1");
   const proxy = await upgrades.deployProxy(CrowdFundingV1,[0]);
   await proxy.deployed();
 
-  console.log(proxy.address);
-  console.log(await upgrades.erc1967.getImplementationAddress(proxy.address)," getImplementationAddress");
-  console.log(await upgrades.erc1967.getAdminAddress(proxy.address)," getAdminAddress");
+  console.log(proxy.address, "-ProxyAddress");
+  console.log(await upgrades.erc1967.getImplementationAddress(proxy.address),"-ImplementationAddress");
+  console.log(await upgrades.erc1967.getAdminAddress(proxy.address),"-AdminAddress");
+
+  // Write to output.txt
+  let parsedFile = envfile.parse(sourcePath);
+  parsedFile.PROXY_ADDRESS = proxy.address;
+  parsedFile.IMPLEMENTATION_ADDRESS = await upgrades.erc1967.getImplementationAddress(proxy.address);
+  parsedFile.ADMIN_ADDRESS = await upgrades.erc1967.getAdminAddress(proxy.address);
+
+  fs.writeFileSync('./output.txt', envfile.stringify(parsedFile));
 }
 
 main().catch((error) => {
--- scripts/upgrade.js
+++ scripts/upgrade.js
@@ -1,16 +1,38 @@
 const hre = require("hardhat");
 const { ethers, upgrades } = require("hardhat");
 require("dotenv").config();
+const fs = require('fs');
+const envfile = require('envfile');
+const sourcePath = './output.txt';
 
-const proxyAddress = process.env.PROXY_ADDRESS;
+
+const content = fs.readFileSync(sourcePath);
+const lines = content.toString().split('\n');
+
+const key_pair = {};
+
+for(let line = 0; line < lines.length; line++){
+  const currentline = lines[line].split('=');
+  key_pair[currentline[0]] = currentline[1];
+}
+
+const proxyAddress = key_pair["PROXY_ADDRESS"];
 
 async function main() {
   const CrowdFundingV2 = await ethers.getContractFactory("CrowdFundingV2");
   const crowdFundingV2 = await upgrades.upgradeProxy(proxyAddress, CrowdFundingV2);
 
-  console.log(crowdFundingV2.address);
-  console.log(await upgrades.erc1967.getImplementationAddress(crowdFundingV2.address)," getImplementationAddress");
-  console.log(await upgrades.erc1967.getAdminAddress(crowdFundingV2.address)," getAdminAddress");
+  console.log(crowdFundingV2.address, "-ProxyAddress");
+  console.log(await upgrades.erc1967.getImplementationAddress(crowdFundingV2.address),"-ImplementationAddress");
+  console.log(await upgrades.erc1967.getAdminAddress(crowdFundingV2.address),"-AdminAddress");
+
+  // Write to output.txt
+  let parsedFile = envfile.parse(sourcePath);
+  parsedFile.PROXY_ADDRESS = crowdFundingV2.address;
+  parsedFile.IMPLEMENTATION_ADDRESS = await upgrades.erc1967.getImplementationAddress(crowdFundingV2.address);
+  parsedFile.ADMIN_ADDRESS = await upgrades.erc1967.getAdminAddress(crowdFundingV2.address);
+
+  fs.writeFileSync('./output.txt', envfile.stringify(parsedFile));
 }
 
 main().catch((error) => {
